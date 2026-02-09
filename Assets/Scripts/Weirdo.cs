612d3b0bd2b72e7213c57631619191a964262270
Dora <doracheng540@gmail.com>
2026-02-09 12:14:06 -0500

    Finish part of Control challenge week 5 Coding Gym

--- Assets/Scripts/Weirdo.cs
+++ Assets/Scripts/Weirdo.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.EventSystems;
+using TMPro;
+//This is week 5 Coding Gym Challenge: Control.
 public class Weirdo : MonoBehaviour
 {
-    public GameObject what; 
+    public TMP_Text text;
+  
+    public Camera gameCam;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +17,22 @@ public class Weirdo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 omg = Mouse.current.position.ReadValue();
-        if ()
+        Vector3 omg = Mouse.current.position.ReadValue();
+        omg.z = 0;
+        Vector3 omg2= Camera.main.ScreenToWorldPoint(omg);
+        if (EventSystem.current.IsPointerOverGameObject()) 
         {
-
+            text.text = "Hello, I am a character from Twisted Wonderland." +
+                " Im strict as HELL";
+        }
+        else
+        {
+            text.text = "...";
+        }
+        if (omg2.y >-5&&omg2.y<0
+            && omg2.x > -9.3 && omg2.x < -4.5)
+        {
+            text.text = "I am also a character from TwIsTeD WoNdErLaNd. I am a nerd techian.";
         }
     
     }
