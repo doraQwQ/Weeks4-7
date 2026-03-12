073595e61eae2204be07f8a97fb7836f587cdb8f
Dora <doracheng540@gmail.com>
2026-03-12 19:48:23 -0400

    Refined the code and ensure that the traps are working

--- Assets/Scripts/DamageHazard.cs
+++ Assets/Scripts/DamageHazard.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DamageHazard : MonoBehaviour
 {
     public int damage = 20;
     public Player player;
+    public UnityEvent OnEnter;
+    public UnityEvent OnLeave;
 
     private SpriteRenderer hazardRenderer;
     private bool wasinHazard= false;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,12 +24,28 @@ public class DamageHazard : MonoBehaviour
         bool isInHazard = hazardRenderer.bounds.Contains(player.transform.position);
         if (isInHazard&&!wasinHazard)
         {
-            player.TakeDam (damage);
             wasinHazard=true;
+            OnEnter.Invoke();
         }
         else if( !isInHazard && wasinHazard)
         {
             wasinHazard = false;
+            OnLeave.Invoke();
         }
     }
+    //if player is in slime hazard turn yes to true
+    public void Slow()
+    {
+        player.speed = 3;
+    }
+    //if player is in trap hazard turn yes2 to true
+    public void Trap()
+    {        
+        player.health-= damage;
+    }
+    public void SlowReverse()
+    {
+        player.speed = 10;
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public int health=100;
+    public int speed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,10 +14,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = transform.position;
+        move.z = 0;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move.y += speed * Time.deltaTime;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            move.x -= speed * Time.deltaTime;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            move.y -= speed * Time.deltaTime;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            move.x += speed * Time.deltaTime;
+        }
+        transform.position = move;
+        Debug.Log(health);
     }
-    public void TakeDam(int damage)
-    {
-        health -= damage;
-    }
+
 }
